51b6a80405ae04baff0fc44073e873b0a09bbcf0
Ammuzds <123801030+Ammuzds@users.noreply.github.com>
2023-05-18 12:56:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,19 +18,19 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ x Alien-Alfa"
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.botname = "𝙎𝘼𝙉𝙆𝙐 𝙀𝙁𝙓 💚x 𝙀𝙑𝘼 𝙀𝙁𝙓 ❤‍🩹"
+global.ownername= "𝙀𝙑𝘼 𝙀𝙁𝙓 ❤‍🩹"
 global.myweb ="https://api-alphabot.herokuapp.com/"
 global.youtube = "https://youtube.com/c/alienalfa"
 global.github = "https://zeeoneofc.github.io/"
-global.ownernomer = "62887435047326"
-global.ownernomerr = "+62887435047326"
+global.ownernomer = "919778707651"
+global.ownernomerr = "+919778707651"
 global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390","447405935355"] //ganti agar fitur owner bisa di gunakan
+global.owner = ["919778707651","919778707651","919778707651","919778707651"] //ganti agar fitur owner bisa di gunakan
 global.packname = '© Alphabot-Mdོ /n' //sticker wm
-global.author = 'Created By ZeeoneOfc & Alien-Alfa' //sticker wm
+global.author = 'Created By 𝙎𝘼𝙉𝙆𝙐 𝙀𝙁𝙓 💚 & 𝙀𝙑𝘼 𝙀𝙁𝙓 ❤‍🩹' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
